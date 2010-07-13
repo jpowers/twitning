1a5ed1e46b0f6c65fe6b7c93e00b0cf2ba091450
Brian Kaney <brian@vermonster.com>
2010-07-12 22:07:10 -0400

    Style updates

--- javascripts/slides.js
+++ javascripts/slides.js
@@ -5,7 +5,7 @@
  */
 
 var numSlides;
-var slideMargin = 30;
+var slideMargin = 100;
 var slideWidth = screen.width + slideMargin;
 var slideHeight = screen.height;
 var secondsPerSlide = 10;
@@ -21,8 +21,9 @@ $(function() {
 function initializeSlideshowStyles() {
   $('#slideshow').width(numSlides * slideWidth);
   $('#slideshow').height(slideHeight);
-  $('section').width(slideWidth - slideMargin);
-  $('section').css('margin-right', slideMargin);
+  $('section').width(slideWidth - slideMargin - slideMargin - slideMargin - 30);
+  $('section').css('margin-right', slideMargin + 30);
+  $('section').css('margin-left', slideMargin);
   $('section').height(slideHeight);
 }
 
@@ -63,14 +64,14 @@ function goToSlide(slideNumber) {
   if (slideNumber > numSlides) {
     slideNumber = numSlides;
   }
-  var targetScrollPosition = (slideNumber-1) * slideWidth;
+  var targetScrollPosition = (slideNumber-1) * (slideWidth - 100);
   $(window).scrollLeft(targetScrollPosition);
 }
 
 function currentSlideNumber() {
   // we add a pixel to avoid a type of fencepost problem
   var currentHorizontalScrollPosition = $(window).scrollLeft() + 1;
-  return Math.ceil(currentHorizontalScrollPosition / slideWidth);
+  return Math.ceil(currentHorizontalScrollPosition / (slideWidth - 100));
 }
 
 /*********************
@@ -81,18 +82,62 @@ function currentSlideNumber() {
 
 function startAutoplay() {
   $('#play').hide();
+  $('#timer').show();; 
   autoAdvanceSlide();
   automaticSlides = setInterval('autoAdvanceSlide()', 1000 * secondsPerSlide);
 }
 
 function autoAdvanceSlide() {
   goToSlide(currentSlideNumber() + 1);
+  $('#timer').html(10); 
   if (currentSlideNumber() == numSlides) {
     stopAutoplay();
+  } else {
+    myCounter.start();
   }
 }
 
 function stopAutoplay() {
   clearInterval(automaticSlides);
   $('#play').show();
+  $('#timer').hide();
 }
+
+/** Counter
+ */
+function Countdown(options) {
+  var timer,
+  instance = this,
+  seconds = options.seconds || 10,
+  updateStatus = options.onUpdateStatus || function () {},
+  counterEnd = options.onCounterEnd || function () {};
+
+  function decrementCounter() {
+    updateStatus(seconds);
+    if (seconds === 0) {
+      counterEnd();
+      instance.stop();
+    }
+    seconds--;
+  }
+
+  this.start = function () {
+    clearInterval(timer);
+    timer = 0;
+    seconds = options.seconds;
+    timer = setInterval(decrementCounter, 1000);
+  };
+
+  this.stop = function () {
+    clearInterval(timer);
+  };
+}
+
+
+var myCounter = new Countdown({  
+    seconds: 9,  // number of seconds to count down
+    onUpdateStatus: function(sec){ $('#timer').html(sec);}, // callback for each second
+    onCounterEnd: function(){ $('#timer').html(10); } // final action
+});
+
+
